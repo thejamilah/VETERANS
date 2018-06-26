4835a8d98bc2fdf411cae48e2aec2b0f852b785c
Mike K <m.kermes89@gmail.com>
2018-06-25 19:38:49 -0700

    added some comments to help build

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 'use strict';
 
-// constructor function for what to pass to the changing form element
+// constructor function for what to pass to the changing form element. this will need work.
 function MakeForm(name){
-  this.name = name;  //name of the button ex: "Education"
+  this.name = name; // name of the button ex: "Education"
   this.type = type; // type of form element: "radio", "checkbox", etc.
   this.id = id; // and ID to html i think we need
   this.class; // possibly the class if we need to pass it class info for css
 }
 
 // switch case build for values returned from form
-
+// 'valueIn' should come from event handler to decide what form was selected.
+// add more cases as needed for the form (there will be a lot)
 switch(valueIn){
 
 case 'education':
@@ -29,3 +30,8 @@ case 'services':
   break;
 }
 
+// event handler for when "Next" on html page is clicked. this should take the value of the forms and pass it to javascript as "valueIn"
+
+
+
+// functions to hide form.... i think functions will be ran in the switch case to hide previous form elements. might need one for showing and hiding each individual form group.
