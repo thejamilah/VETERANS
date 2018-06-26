1b484d60f51b08b265e23e4195e788141aba1479
Mike K <m.kermes89@gmail.com>
2018-06-25 19:04:30 -0700

    building place holder constructor and switch case

---
+++ js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// constructor function for what to pass to the changing form element
+function MakeForm(name){
+  this.name = name;
+  this.type = type;
+  this.id = id;
+  this.class;
+}
+
+// switch case build for values returned from form
+
+switch(valueIn){
+
+case 'education':
+  //code to change the form element to what we need after education is chosen
+  break;
+
+case 'h-n-w':
+  //run some code here
+  break;
+
+case 'homelessness':
+  //run some code here;
+  break;
+
+case 'services':
+  //run some code here
+  break;
+}
+
