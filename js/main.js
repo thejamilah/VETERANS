f0ee209652846d23f18b0a0e0842a40dacc33849
Mike K <m.kermes89@gmail.com>
2018-06-25 20:51:24 -0700

    added hide form code for starting cases

--- js/main.js
+++ js/main.js
@@ -14,19 +14,21 @@ function MakeForm(name, type, id){
 switch(valueIn){
 
 case 'education':
-  //code to change the form element to what we need after education is chosen
+  document.getElementById('startForm').style.display='none';
+  document.getElementById('showEduForm').style.display='block';
   break;
 
 case 'h-n-w':
-  //run some code here
+  document.getElementById('startForm').style.display='none';
+  document.getElementById('h-n-w').style.display='block';
   break;
 
-case 'homelessness':
-  //run some code here;
+case 'employment':
+  document.getElementById('startForm').style.display='none';
   break;
 
 case 'services':
-  //run some code here
+  document.getElementById('startForm').style.display='none';
   break;
 }
 
